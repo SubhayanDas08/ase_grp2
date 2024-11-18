79c04a4301f0e43e3c55fa8842011a9b0c0c51c1
Boris Stavisky <bor2002isky@gmail.com>
2024-11-18 17:09:24 +0000

    Updated map component

--- frontend/apps/desktop/src/pages/Map.tsx
+++ frontend/apps/desktop/src/pages/Map.tsx
@@ -1,7 +1,9 @@
+import MapVisual from "../components/mapVisual";
+
 export default function Map() {
     return(
-        <div className="bg-red-500 h-96">
-            Map
+        <div className="bg-red-500 h-full">
+            <MapVisual />
         </div>
     )
 }
