096c8f2a123260121b264c06b0c262af66782b78
Boris Stavisky <bor2002isky@gmail.com>
2024-11-18 12:47:54 +0000

    Formatting

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -11,19 +11,19 @@ import "./App.css";
 export default function App() {
     return (
         <Router>
-        <div className="flex h-screen">
-            <div className="h-full w-24 fixed">
-            <Sidebar />
+            <div className="flex h-screen">
+                <div className="h-full w-24 fixed">
+                <Sidebar />
+                </div>
+                <div className="ml-24 flex-1 overflow-y-auto p-5 ">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/map" element={<Map />} />
+                    <Route path="/server" element={<Server />} />
+                    <Route path="/settings" element={<Settings />} />
+                </Routes>
+                </div>
             </div>
-            <div className="ml-24 flex-1 overflow-y-auto p-5 ">
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/map" element={<Map />} />
-                <Route path="/server" element={<Server />} />
-                <Route path="/settings" element={<Settings />} />
-            </Routes>
-            </div>
-        </div>
         </Router>
     );
 }
