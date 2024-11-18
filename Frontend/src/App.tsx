3b589cf4ee31c3bd0f067a9687bcf241eb4f7ed0
Boris Stavisky <bor2002isky@gmail.com>
2024-11-18 12:47:08 +0000

    Updated sidebar to be constantly fixed, while main content to be scrollable and height is content dependant

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -9,21 +9,22 @@ import Sidebar from "./components/sidebar.tsx";
 import "./App.css";
 
 export default function App() {
-  return (
-    <Router>
-      <div className="flex h-screen">
-          <div className="flex h-full w-24">
-              <Sidebar />
-          </div>
-          <div className="h-full w-full p-5">
-              <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/map" element={<Map />} />
-                  <Route path="/server" element={<Server />} />
-                  <Route path="/settings" element={<Settings />} />
-              </Routes>
-          </div>
-      </div>
-  </Router>
-  )
+    return (
+        <Router>
+        <div className="flex h-screen">
+            <div className="h-full w-24 fixed">
+            <Sidebar />
+            </div>
+            <div className="ml-24 flex-1 overflow-y-auto p-5 ">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/map" element={<Map />} />
+                <Route path="/server" element={<Server />} />
+                <Route path="/settings" element={<Settings />} />
+            </Routes>
+            </div>
+        </div>
+        </Router>
+    );
 }
+  
