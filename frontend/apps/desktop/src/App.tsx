a530a9fa970a7993b07b66ad6e20f8eded3bff9e
Boris Stavisky <bor2002isky@gmail.com>
2024-11-18 16:21:47 +0000

    Small change

--- frontend/apps/desktop/src/App.tsx
+++ frontend/apps/desktop/src/App.tsx
@@ -13,15 +13,15 @@ export default function App() {
         <Router>
             <div className="flex h-screen">
                 <div className="h-full w-24 fixed">
-                <Sidebar />
+                    <Sidebar />
                 </div>
                 <div className="ml-24 flex-1 overflow-y-auto p-5 ">
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/map" element={<Map />} />
-                    <Route path="/server" element={<Server />} />
-                    <Route path="/settings" element={<Settings />} />
-                </Routes>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/map" element={<Map />} />
+                        <Route path="/server" element={<Server />} />
+                        <Route path="/settings" element={<Settings />} />
+                    </Routes>
                 </div>
             </div>
         </Router>
