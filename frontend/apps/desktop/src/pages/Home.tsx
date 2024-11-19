4af35b1f5534b09f8fbaf57cfca9854effff483e
Agathe27 <151518183+Agathe27@users.noreply.github.com>
2024-11-19 14:09:32 +0000

    adding_searchbar

--- frontend/apps/desktop/src/pages/Home.tsx
+++ frontend/apps/desktop/src/pages/Home.tsx
@@ -1,21 +1,30 @@
+//import {useState} from "react";
+import TextField from '@mui/material/TextField';
+
+
 export default function Home() {
     return(
         <div className="bg-red-500">
-            <div className="h-52">
-                hello !! 
-            </div>
-            <div className="h-52">
-                hello
-            </div>
-            <div className="h-52">
-                hello
-            </div>
-            <div className="h-52">
-                hello
-            </div>
-            <div className="h-52">
-                hello
-            </div>
+            <div className="w-1/2 items-center">
+                <TextField
+                id="outlined-basic"
+                variant="outlined"
+                fullWidth
+                label="Search"
+                />
+        </div>
+        <div className="h-52">
+            hello
+        </div>
+        <div className="h-52">
+            hello
+        </div>
+        <div className="h-52">
+            hello
+        </div>
+        <div className="h-52">
+            hello
+        </div>
         </div>
     )
 }
