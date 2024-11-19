cdc411ca637a79fa86781e9f468e40294a0a1f60
Boris Stavisky <bor2002isky@gmail.com>
2024-11-19 12:57:59 +0000

    test

--- frontend/apps/desktop/src/pages/Map.tsx
+++ frontend/apps/desktop/src/pages/Map.tsx
@@ -2,7 +2,7 @@ import MapVisual from "../components/mapVisual";
 
 export default function Map() {
     return(
-        <div className="h-full">
+        <div className="h-full bg-yellow-500">
             <MapVisual />
         </div>
     )
