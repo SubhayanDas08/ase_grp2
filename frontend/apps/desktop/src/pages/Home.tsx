fa40cc4143bcebe312f4b0ff7767634e6722d579
Agathe27 <151518183+Agathe27@users.noreply.github.com>
2024-11-19 12:55:48 +0000

    update

--- frontend/apps/desktop/src/pages/Home.tsx
+++ frontend/apps/desktop/src/pages/Home.tsx
@@ -2,7 +2,7 @@ export default function Home() {
     return(
         <div className="bg-red-500">
             <div className="h-52">
-                hello
+                hello !! 
             </div>
             <div className="h-52">
                 hello
