77590c26f418b072066549e194c90a0be302d2a8
Agathe27 <151518183+Agathe27@users.noreply.github.com>
2025-02-06 17:52:52 +0000

    Login updates

--- frontend/apps/desktop/src/pages/Login.tsx
+++ frontend/apps/desktop/src/pages/Login.tsx
@@ -1,45 +1,55 @@
 import Logo from "../assets/Logo.svg";
+import GoogleLogo from "../assets/GoogleLogo.svg";
 
 export default function Login() {
     return (
         <div className="flex h-full w-full">
-            <div className="flex justify-center h-full w-1/2 bg-red-400">
-                <div  className="flex flex-col justify-center h-full w-1/2 bg-red-300">
-                    <div className="h-2/3 bg-green-300 ">
-                        <div className="text-center bg-green-300 h-1/5">
-                            <div className="text-2xl" style={{color: "#029AA5"}}>
+            <div className="flex justify-center h-full w-1/2">
+                <div  className="flex flex-col justify-center h-full w-2/3">
+                    <div className="h-2/3">
+                        <div className="text-center h-1/5">
+                            <div className="text-2xl font-bold" style={{color: "#029AA5"}}>
                                 Login to your account
                             </div>
-                            <div>
+                            <div className= "text-xs" style={{color:"#70706F"}}>
                                 Welcome back ! Please enter your details.
                             </div>
                         </div>
-                        <div className="text-center bg-red-300 h-1/3">
-                            <div className="h-1/2">
-                                email
+                        <div className="text-center h-1/2">
+                            <div className="flex justify-center  items-center h-1/3">
+                                <input placeholder="Email" className="flex items-center loginRegistrationForm ps-5" style={{color:"#70706F50"}}/>
                             </div>
-                            <div className="h-1/2">
-                                password
+                            <div className="flex justify-center  items-center h-1/3">
+                                <input placeholder="Password" className="flex items-center loginRegistrationForm ps-5" style={{color:"#029AA5"}}/>
                             </div>
-                        </div>
-                        <div className="text-center bg-red-200 h-1/3">
-                            <div className="h-1/3">
-                                Log In button
+                            <div className="flex justify-center  items-center h-1/3 ">
+                                <button className="loginRegistrationButton text-white">
+                                    Log In
+                                </button>
                             </div> 
-                            <div className="h-1/3">
+                        </div>
+                         <div className="text-center h-1/4">
+                            <div className="h-1/3 flex justify-center items-center">
                                 or
                             </div>
-                            <div className="h-1/3">
-                                Log in w Google
+                            <div className="h-2/3 flex justify-center items-center">
+                                <button className="loginGoogleRegistrationButton flex flex-row justify-center items-center">
+                                    <div>
+                                        <img src={GoogleLogo} alt="GoogleLogo" />
+                                    </div>
+                                    <span style={{marginLeft:"7px"}}>
+                                        Log In with Google
+                                    </span>
+                                </button>
                             </div>
                         </div>
-                        <div className="flex flex-row bg-red-100 h-1/6">
-                            <div className="bg-green-300 w-2/3 text-center">
-                                Don't have an account?
-                            </div>
-                            <div className="bg-green-400 w-1/3" style={{color: "#029AA5"}}>
+                        <div className="text-center h-1/6">
+                            <span style={{color:"#70706F"}}>
+                                Don't have an account? 
+                            </span>
+                            <span style={{color:"#029AA5", marginLeft:"5px"}}>
                                 Sign up
-                            </div>
+                            </span>
                         </div>
                         
                     </div>
