fbe4baaf1e8e12fbd123e8eb5cf047847739c561
Boris Stavisky <bor2002isky@gmail.com>
2025-02-07 12:49:27 +0000

    Updated login page and CSS classes

--- frontend/apps/desktop/src/pages/Login.tsx
+++ frontend/apps/desktop/src/pages/Login.tsx
@@ -8,54 +8,52 @@ export default function Login() {
                 <div  className="flex flex-col justify-center h-full w-2/3">
                     <div className="h-2/3">
                         <div className="text-center h-1/5">
-                            <div className="text-2xl font-bold" style={{color: "#029AA5"}}>
-                                Login to your account
+                            <div className="titleText primaryColor1">
+                                Log In to your account
                             </div>
-                            <div className= "text-xs" style={{color:"#70706F"}}>
-                                Welcome back ! Please enter your details.
+                            <div className= "miniText secondaryColor">
+                                Welcome back! Please enter your details.
                             </div>
                         </div>
                         <div className="text-center h-1/2">
-                            <div className="flex justify-center  items-center h-1/3">
-                                <input placeholder="Email" className="flex items-center loginRegistrationForm ps-5" style={{color:"#70706F50"}}/>
+                            <div className="flex justify-center items-center h-1/3">
+                                <input placeholder="Email" className="flex items-center loginRegistrationFormInput"/>
                             </div>
-                            <div className="flex justify-center  items-center h-1/3">
-                                <input placeholder="Password" className="flex items-center loginRegistrationForm ps-5" style={{color:"#029AA5"}}/>
+                            <div className="flex justify-center items-center h-1/3">
+                                <input placeholder="Password" className="flex items-center loginRegistrationFormInput"/>
                             </div>
-                            <div className="flex justify-center  items-center h-1/3 ">
-                                <button className="loginRegistrationButton text-white">
+                            <div className="flex justify-center items-center h-1/3 ">
+                                <button className="loginRegistrationButton">
                                     Log In
                                 </button>
                             </div> 
                         </div>
                          <div className="text-center h-1/4">
-                            <div className="h-1/3 flex justify-center items-center">
-                                or
+                            <div className="h-1/3 flex justify-center items-center secondaryColor">
+                                Or
                             </div>
                             <div className="h-2/3 flex justify-center items-center">
                                 <button className="loginGoogleRegistrationButton flex flex-row justify-center items-center">
                                     <div>
                                         <img src={GoogleLogo} alt="GoogleLogo" />
                                     </div>
-                                    <span style={{marginLeft:"7px"}}>
+                                    <span className="ml-2">
                                         Log In with Google
                                     </span>
                                 </button>
                             </div>
                         </div>
-                        <div className="text-center h-1/6">
-                            <span style={{color:"#70706F"}}>
+                        <div className="text-center h-1/6 miniText">
+                            <span className="secondaryColor">
                                 Don't have an account? 
                             </span>
-                            <span style={{color:"#029AA5", marginLeft:"5px"}}>
+                            <span className="primaryColor1 ml-1">
                                 Sign up
                             </span>
                         </div>
-                        
                     </div>
                 </div>
             </div>
-
             <div className="flex flex-col h-full w-1/2">
                 <div className="flex justify-end h-2/3 w-full">
                     <img src={Logo} alt="Logo" />
