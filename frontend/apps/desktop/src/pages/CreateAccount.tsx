7f67d89378a3b55c78469f859b88c97ddbab3c03
Agathe27 <151518183+Agathe27@users.noreply.github.com>
2025-02-13 16:03:27 +0000

    Height responsiveness added

--- frontend/apps/desktop/src/pages/CreateAccount.tsx
+++ frontend/apps/desktop/src/pages/CreateAccount.tsx
@@ -33,69 +33,67 @@ export default function CreateAccount() {
     };
 
     return (
-        <div className="flex h-full w-full">
-            <div className="flex flex-col justify-center h-full lg:w-1/2 w-full">
-                <div  className="flex flex-col justify-center h-full w-full">
-                    <div className="flex justify-end h-16 w-full">
-                        <img src={FullLogo} alt="FullLogo" className="lg:h-0 lg:w-0 h-full w-full" />
+        <div className="flex h-full w-full overflow-y-auto">
+            <div className="flex flex-col justify-center h-full lg:w-1/2 w-full" style={{minHeight: "800px"}}>
+                <div className="flex justify-end h-16 w-full">
+                    <img src={FullLogo} alt="FullLogo" className="lg:h-0 lg:w-0 h-full w-full" />
+                </div>
+                <div className="h-3/4">
+                    <div className="text-center h-1/5">
+                        <div className="titleText primaryColor1">
+                            Create an Account
+                        </div>
+                        <div className= "miniText secondaryColor">
+                            Welcome! Please login or sign up to our page
+                        </div>
                     </div>
-                    <div className="h-3/4">
-                        <div className="text-center h-1/5">
-                            <div className="titleText primaryColor1">
-                                Create an Account
-                            </div>
-                            <div className= "miniText secondaryColor">
-                                Welcome! Please login or sign up to our page
+                    <div className="text-center h-1/2"> 
+                        <div className="flex justify-center items-center h-1/5">
+                            <div className="flex justify-between gap-4" style={{width: "70%"}}> 
+                                <input type="text" name="firstName" placeholder="First Name" className="nameFormInput" onChange={handleFormData}/>
+                                <input type="text" name="lastName" placeholder="Last Name" className="nameFormInput" value={formData.lastName} onChange={handleFormData}/>
                             </div>
                         </div>
-                        <div className="text-center h-1/2"> 
-                            <div className="flex justify-center items-center h-1/5">
-                                <div className="flex justify-between gap-4" style={{width: "70%"}}> 
-                                    <input type="text" name="firstName" placeholder="First Name" className="nameFormInput" onChange={handleFormData}/>
-                                    <input type="text" name="lastName" placeholder="Last Name" className="nameFormInput" value={formData.lastName} onChange={handleFormData}/>
+                        <div className="flex justify-center items-center h-1/5">
+                            <input type="text" name="email" placeholder="Email" className="flex items-center loginRegistrationFormInput" value={formData.email} onChange={handleFormData}/>
+                        </div>
+                        <div className="flex justify-center items-center h-1/5">
+                            <input type="text" name="phoneNb" placeholder="Phone Number" className="flex items-center loginRegistrationFormInput " value={formData.phoneNb} onChange={handleFormData}/>
+                        </div>
+                        <div className="flex justify-center items-center h-1/5">
+                            <input name="password" type={showPassword ? "text" : "password"} placeholder="Password" className="loginRegistrationFormInput ms-6" value={formData.password} onChange={handleFormData}/>
+                            <button type="button" onClick={() => setShowPassword(!showPassword)} className="-translate-x-10">
+                                {showPassword ? (<EyeOff className="w-6 h-6 secondaryColor" /> ) : (<Eye className="w-6 h-6 secondaryColor" />)}
+                            </button>
+                        </div>
+                        <div className="flex justify-center items-center h-1/5">
+                            <button className="loginRegistrationButton" onClick={() => console.log(formData)}>
+                                Sign up
+                            </button>
+                        </div> 
+                    </div>
+                    <div className="text-center h-1/4">
+                        <div className="h-2/5 flex justify-center items-center secondaryColor">
+                            Or
+                        </div>
+                        <div className="h-3/5 flex justify-center items-center">
+                            <button className="loginGoogleRegistrationButton flex flex-row justify-center items-center">
+                                <div>
+                                    <img src={GoogleLogo} alt="GoogleLogo" />
                                 </div>
-                            </div>
-                            <div className="flex justify-center items-center h-1/5">
-                                <input type="text" name="email" placeholder="Email" className="flex items-center loginRegistrationFormInput" value={formData.email} onChange={handleFormData}/>
-                            </div>
-                            <div className="flex justify-center items-center h-1/5">
-                                <input type="text" name="phoneNb" placeholder="Phone Number" className="flex items-center loginRegistrationFormInput " value={formData.phoneNb} onChange={handleFormData}/>
-                            </div>
-                            <div className="flex justify-center items-center h-1/5">
-                                <input name="password" type={showPassword ? "text" : "password"} placeholder="Password" className="loginRegistrationFormInput ms-6" value={formData.password} onChange={handleFormData}/>
-                                <button type="button" onClick={() => setShowPassword(!showPassword)} className="-translate-x-10">
-                                    {showPassword ? (<EyeOff className="w-6 h-6 secondaryColor" /> ) : (<Eye className="w-6 h-6 secondaryColor" />)}
-                                </button>
-                            </div>
-                            <div className="flex justify-center items-center h-1/5">
-                                <button className="loginRegistrationButton" onClick={() => console.log(formData)}>
-                                    Sign up
-                                </button>
-                            </div> 
+                                <span className="ml-2">
+                                    Log In with Google
+                                </span>
+                            </button>
                         </div>
-                        <div className="text-center h-1/4">
-                            <div className="h-2/5 flex justify-center items-center secondaryColor">
-                                Or
-                            </div>
-                            <div className="h-3/5 flex justify-center items-center">
-                                <button className="loginGoogleRegistrationButton flex flex-row justify-center items-center">
-                                    <div>
-                                        <img src={GoogleLogo} alt="GoogleLogo" />
-                                    </div>
-                                    <span className="ml-2">
-                                        Log In with Google
-                                    </span>
-                                </button>
-                            </div>
-                        </div>
-                        <div className="text-center h-1/6 miniText">
-                            <span className="secondaryColor">
-                                Already have an account? 
-                            </span>
-                            <Link to="/" className="primaryColor1 ml-1 hover:underline">
-                                Log In
-                            </Link>
-                        </div>
+                    </div>
+                    <div className="text-center h-1/6 miniText">
+                        <span className="secondaryColor">
+                            Already have an account? 
+                        </span>
+                        <Link to="/" className="primaryColor1 ml-1 hover:underline">
+                            Log In
+                        </Link>
                     </div>
                 </div>
             </div>
--- frontend/apps/desktop/src/pages/Login.tsx
+++ frontend/apps/desktop/src/pages/Login.tsx
@@ -26,60 +26,58 @@ export default function Login() {
     };
 
     return (
-        <div className="flex h-full w-full">
-            <div className="flex flex-col justify-center h-full lg:w-1/2 w-full">
-                <div  className="flex flex-col justify-center h-full w-full">
-                    <div className="flex justify-end h-16 w-full">
-                        <img src={FullLogo} alt="FullLogo" className="lg:h-0 lg:w-0 h-full w-full" />
+        <div className="flex h-full w-full overflow-y-auto">
+            <div className="flex flex-col justify-center h-full lg:w-1/2 w-full" style={{minHeight: "800px"}}>
+                <div className="flex justify-end h-16 w-full">
+                    <img src={FullLogo} alt="FullLogo" className="lg:h-0 lg:w-0 h-full w-full" />
+                </div>
+                <div className="h-2/3">
+                    <div className="text-center h-1/5">
+                        <div className="titleText primaryColor1">
+                            Log In to your account
+                        </div>
+                        <div className= "miniText secondaryColor">
+                            Welcome back! Please enter your details.
+                        </div>
                     </div>
-                    <div className="h-2/3">
-                        <div className="text-center h-1/5">
-                            <div className="titleText primaryColor1">
-                                Log In to your account
-                            </div>
-                            <div className= "miniText secondaryColor">
-                                Welcome back! Please enter your details.
-                            </div>
+                    <div className="text-center h-1/2">
+                        <div className="flex justify-center items-center h-1/3">
+                            <input type="text" name="email" placeholder="Email" className="flex items-center loginRegistrationFormInput" value={formData.email} onChange={handleFormData}/>
                         </div>
-                        <div className="text-center h-1/2">
-                            <div className="flex justify-center items-center h-1/3">
-                                <input type="text" name="email" placeholder="Email" className="flex items-center loginRegistrationFormInput" value={formData.email} onChange={handleFormData}/>
-                            </div>
-                            <div className="flex justify-center items-center h-1/3">
-                                <input name="password" type={showPassword ? "text" : "password"} placeholder="Password" className="loginRegistrationFormInput ms-6" value={formData.password} onChange={handleFormData}/>
-                                <button type="button" onClick={() => setShowPassword(!showPassword)} className="-translate-x-10">
-                                    {showPassword ? (<EyeOff className="w-6 h-6 secondaryColor" /> ) : (<Eye className="w-6 h-6 secondaryColor" />)}
-                                </button>
-                            </div>
-                            <div className="flex justify-center items-center h-1/3 ">
-                                <button className="loginRegistrationButton"  onClick={() => console.log(formData)}>
-                                    Log In
-                                </button>
-                            </div> 
+                        <div className="flex justify-center items-center h-1/3">
+                            <input name="password" type={showPassword ? "text" : "password"} placeholder="Password" className="loginRegistrationFormInput ms-6" value={formData.password} onChange={handleFormData}/>
+                            <button type="button" onClick={() => setShowPassword(!showPassword)} className="-translate-x-10">
+                                {showPassword ? (<EyeOff className="w-6 h-6 secondaryColor" /> ) : (<Eye className="w-6 h-6 secondaryColor" />)}
+                            </button>
                         </div>
-                         <div className="text-center h-1/4">
-                            <div className="h-1/3 flex justify-center items-center secondaryColor">
-                                Or
-                            </div>
-                            <div className="h-2/3 flex justify-center items-center">
-                                <button className="loginGoogleRegistrationButton flex flex-row justify-center items-center">
-                                    <div>
-                                        <img src={GoogleLogo} alt="GoogleLogo" />
-                                    </div>
-                                    <span className="ml-2">
-                                        Log In with Google
-                                    </span>
-                                </button>
-                            </div>
+                        <div className="flex justify-center items-center h-1/3 ">
+                            <button className="loginRegistrationButton"  onClick={() => console.log(formData)}>
+                                Log In
+                            </button>
+                        </div> 
+                    </div>
+                        <div className="text-center h-1/4">
+                        <div className="h-1/3 flex justify-center items-center secondaryColor">
+                            Or
                         </div>
-                        <div className="text-center h-1/6 miniText">
-                            <span className="secondaryColor">
-                                Don't have an account? 
-                            </span>
-                            <Link to="/create_account" className="primaryColor1 ml-1 hover:underline">
-                                Sign up
-                            </Link>
+                        <div className="h-2/3 flex justify-center items-center">
+                            <button className="loginGoogleRegistrationButton flex flex-row justify-center items-center">
+                                <div>
+                                    <img src={GoogleLogo} alt="GoogleLogo" />
+                                </div>
+                                <span className="ml-2">
+                                    Log In with Google
+                                </span>
+                            </button>
                         </div>
+                    </div>
+                    <div className="text-center h-1/6 miniText">
+                        <span className="secondaryColor">
+                            Don't have an account? 
+                        </span>
+                        <Link to="/create_account" className="primaryColor1 ml-1 hover:underline">
+                            Sign up
+                        </Link>
                     </div>
                 </div>
             </div>
