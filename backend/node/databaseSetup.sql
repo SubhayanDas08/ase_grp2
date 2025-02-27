d6885fb75e5aacbb216c1d255d5a847405b077b1
Simon Walter <simon@walter-wm.de>
2025-02-27 12:52:12 +0000

    chore(databaseSetup): create example data

--- backend/node/databaseSetup.sql
+++ backend/node/databaseSetup.sql
@@ -6,7 +6,6 @@ CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(100) UNIQUE NOT NULL,
     password TEXT NOT NULL,
     phone_number VARCHAR(20) NOT NULL,
-    role VARCHAR(20) NOT NULL DEFAULT 'User',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -19,6 +18,73 @@ CREATE TABLE IF NOT EXISTS locations (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Create Events Table
+CREATE TABLE IF NOT EXISTS events (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  start_date TIMESTAMP NOT NULL,
+  end_date TIMESTAMP NOT NULL,
+  created_by VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create Roles Table
+CREATE TABLE IF NOT EXISTS roles (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create Permissions Table
+CREATE TABLE IF NOT EXISTS permissions (
+    id SERIAL PRIMARY KEY,
+    permission VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create RolePermissions Table
+CREATE TABLE IF NOT EXISTS role_permissions (
+    role_id INT NOT NULL,
+    permission_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (role_id, permission_id),
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id),
+    CONSTRAINT fk_permission FOREIGN KEY (permission_id) REFERENCES permissions(id)
+);
+
+-- Create DomainRoles Table
+CREATE TABLE IF NOT EXISTS domain_roles (
+    domain VARCHAR(100) NOT NULL,
+    role_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (domain, role_id),
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id)
+);
+
 -- Create Indexes for Performance
-CREATE INDEX idx_users_email ON users(email);
-CREATE INDEX idx_locations_name ON locations(name);
+CREATE INDEX IF NOT EXISTS idx_users_email ON users(email);
+CREATE INDEX IF NOT EXISTS idx_locations_name ON locations(name);
+
+
+-- Create Example Data
+-- Insert role
+INSERT INTO roles (name) VALUES ('garda');
+INSERT INTO roles (name) VALUES ('citymanager');
+INSERT INTO roles (name) VALUES ('emergencyservices');
+INSERT INTO roles (name) VALUES ('transportadministator');
+INSERT INTO roles (name) VALUES ('wastermanager');
+
+-- Insert an example permission
+INSERT INTO permissions (permission) VALUES ('create_events');
+INSERT INTO permissions (permission) VALUES ('delete_events');
+
+-- Insert an example role_permission
+INSERT INTO role_permissions (role_id, permission_id) 
+VALUES (
+    (SELECT id FROM roles WHERE name = 'garda'),
+    (SELECT id FROM permissions WHERE permission = 'create_events')
+);
+
+-- Insert an example domain_role
+INSERT INTO domain_roles (domain, role_id)
+VALUES ('garda.ie', (SELECT id FROM roles WHERE name = 'garda'));
