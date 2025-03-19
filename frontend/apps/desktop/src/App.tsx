fcaea888b4510929fe9e8744ef5f4fa5eb7be718
Boris Stavisky <bor2002isky@gmail.com>
2025-03-19 12:38:44 +0000

    Finished sidebar

--- frontend/apps/desktop/src/App.tsx
+++ frontend/apps/desktop/src/App.tsx
@@ -1,17 +1,21 @@
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "leaflet/dist/leaflet.css";
-import Home from "./pages/Home.tsx";
-import WeatherMap from "./pages/WeatherMap.tsx";
-import Events from "./pages/Events.tsx";
-import Settings from "./pages/Settings.tsx";
-import Login from "./pages/Login.tsx";
-import CreateAccount from "./pages/CreateAccount.tsx";
-import Routing from "./pages/Routing.tsx";
-
-import Sidebar from "./components/sidebar.tsx";
 import "./styles/App.css";
-import { useState } from "react";
+
+import CreateAccount from "./pages/CreateAccount.tsx";
+import Login from "./pages/Login.tsx";
+import Sidebar from "./components/sidebar.tsx";
+import Home from "./pages/Home.tsx";
+import Routing from "./pages/Routing.tsx";
+import Events from "./pages/Events.tsx";
+import Traffic from "./pages/Traffic.tsx";
+import Waste from "./pages/Waste.tsx";
+import WeatherMap from "./pages/WeatherMap.tsx";
+import FleetSize from "./pages/FleetSize.tsx";
+import Settings from "./pages/Settings.tsx";
+
 
 export default function App() {
     const [userAuthenticated, setUserAuthenticated] = useState<Boolean>(true);
@@ -33,11 +37,13 @@ export default function App() {
                         <div className="h-full grow ml-[250px] p-5">
                             <Routes>
                                 <Route path="/" element={<Home />} />
-                                <Route path="/weather" element={<WeatherMap />} />
-                                <Route path="/events" element={<Events />} />
                                 <Route path="/routing" element={<Routing />} />
+                                <Route path="/events" element={<Events />} />
+                                <Route path="/traffic" element={<Traffic />} />
+                                <Route path="/waste" element={<Waste />} />
+                                <Route path="/weather" element={<WeatherMap />} />
+                                <Route path="/fleetsize" element={<FleetSize />} />
                                 <Route path="/settings" element={<Settings setUserAuthenticated={setUserAuthenticated} />} />
-                                
                             </Routes>
                         </div>
                     </div>
