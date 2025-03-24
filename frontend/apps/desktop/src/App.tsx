55819669d980bc206359503e4e46fd9f9b413a6e
Boris Stavisky <bor2002isky@gmail.com>
2025-03-24 12:19:08 +0000

    Small fix

--- frontend/apps/desktop/src/App.tsx
+++ frontend/apps/desktop/src/App.tsx
@@ -86,7 +86,7 @@ export default function App() {
                                           path={route} 
                                           element={
                                             <ProtectedRoute>
-                                              pageRouteItemsList[index]
+                                              {pageRouteItemsList[index]}
                                             </ProtectedRoute>
                                           } 
                                           key={index} 
