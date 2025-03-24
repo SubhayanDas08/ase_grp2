cb83b4072f934aa72ee0f11d7668c08d9de6b70a
Agathe27 <151518183+Agathe27@users.noreply.github.com>
2025-03-24 12:10:24 +0000

    adding imports

--- frontend/apps/desktop/src/pages/Waste.tsx
+++ frontend/apps/desktop/src/pages/Waste.tsx
@@ -1,3 +1,8 @@
+import { useState, useEffect } from 'react';
+import { HiOutlineLightningBolt } from "react-icons/hi";
+import { FiCloudLightning } from "react-icons/fi";
+
+
 export default function Waste() {
     return (
         <h1>Waste</h1>
