80593f8657e1a0974a5a8e33e0efbb65a29593b3
Simon Walter <67228030+siwalt1@users.noreply.github.com>
2025-04-03 16:37:32 +0100

    feat: updateName api (#45)

--- backend/node/routes/userRoutes.ts
+++ backend/node/routes/userRoutes.ts
@@ -6,7 +6,8 @@ import {
   FErefreshToken,
   getLocationByIp,
   getCurrentUser,
-  changeUserPassword
+  changeUserPassword,
+  updateFirstAndLastName,
 } from "../controllers/userController";
 import { authenticate } from "../middleware/authenticate";
 const router: Router = express.Router();
@@ -16,7 +17,8 @@ router.post("/login", FElogin);
 router.post("/logout", authenticate(), FElogout);
 router.post("/refresh", FErefreshToken);
 router.get("/locationByIp", authenticate(), getLocationByIp);
-router.get("/get",authenticate(),getCurrentUser);
-router.post("/changePassword",authenticate(),changeUserPassword);
+router.get("/get", authenticate(), getCurrentUser);
+router.post("/changePassword", authenticate(), changeUserPassword);
+router.post("/updateName", authenticate(), updateFirstAndLastName);
 
 export default router;
