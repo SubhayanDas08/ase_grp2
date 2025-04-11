d71e91ae179cfe09ae063801c019d7a5b8275395
Agathe27 <151518183+Agathe27@users.noreply.github.com>
2025-04-11 09:29:20 +0100

    frontend tests ok

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,9 +1,12 @@
-module.exports = {
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
-  testEnvironment: "jest-environment-jsdom", 
+export default {
+  testEnvironment: "jest-environment-jsdom",
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.(ts|tsx)$": ["ts-jest", { useESM: true }],
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  preset: "ts-jest",
+  preset: "ts-jest/presets/js-with-ts-esm",
+  extensionsToTreatAsEsm: [".ts", ".tsx"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1"
+  }
 };
