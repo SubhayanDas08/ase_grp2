4c483ec3c83688b8ac99d66436bc2d238f075c9b
Abhigyan Khaund <160063176+DevAbhi99@users.noreply.github.com>
2025-04-13 11:26:50 +0100

    all errors fixed

--- frontend/apps/mobile/App.js
+++ frontend/apps/mobile/App.js
@@ -8,13 +8,7 @@ import MapVisual from './Components/MapVisual';
 import Traffic from './Components/Traffic';
 import EfficientRoutes from './Components/EfficientRoutes';
 import Events from './Components/Events';
-import EventDetails from './Components/EventDetails';
 import Weather from './Components/Weather';
-import Login from './Components/Login';
-import Signup from './Components/Signup';
-import UserProfile from './Components/UserProfile';
-import Settings from './Components/Settings';
-import ReportIssue from './Components/ReportIssue';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
@@ -34,18 +28,13 @@ export default function App() {
               }}
             >
               <Stack.Screen name="homePage" component={HomePage} />
-              <Stack.Screen name="Menu" component={Menu} />
-              <Stack.Screen name="Login" component={Login} />
-              <Stack.Screen name="Signup" component={Signup} />
+              <Stack.Screen name="Menu" component={Menu} />       
               <Stack.Screen name="MapVisual" component={MapVisual} />
               <Stack.Screen name="Traffic" component={Traffic} />
               <Stack.Screen name="EfficientRoutes" component={EfficientRoutes} />
               <Stack.Screen name="Events" component={Events} />
-              <Stack.Screen name="EventDetails" component={EventDetails} />
+            
               <Stack.Screen name="Weather" component={Weather} />
-              <Stack.Screen name="UserProfile" component={UserProfile} />
-              <Stack.Screen name="Settings" component={Settings} />
-              <Stack.Screen name="ReportIssue" component={ReportIssue} />
             </Stack.Navigator>
           </NavigationContainer>
         </View>
